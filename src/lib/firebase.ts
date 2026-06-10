a615989ee9419b0ef037efe12c657dbb2317233a
patmvp05 <pat.lucero@icloud.com>
2026-06-10 11:29:51 +0700

    refactor: migrate firebase config to json file
    Remove hardcoded config from TypeScript to external JSON file to avoid tooling interference and standardize configuration loading.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -32,7 +32,7 @@ import {
   Query,
   Firestore
 } from 'firebase/firestore';
-import firebaseConfig from './firebaseConfig';
+import firebaseConfig from '../../firebase-applet-config.json';
 
 // Simple check to see if we have valid config
 const isConfigValid = firebaseConfig && firebaseConfig.apiKey && firebaseConfig.apiKey !== 'placeholder';
